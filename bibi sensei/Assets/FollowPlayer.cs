7f6b4cc69e97c1964016951e519fe5b8786edf8a
Toster <tostermocy@gmail.com>
2020-01-25 20:10:52 +0100

    camera follow

---
+++ bibi sensei/Assets/FollowPlayer.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    [SerializeField] private GameObject player;
+
+    private Transform _playerTransform;
+    [SerializeField] private float offset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _playerTransform = player.transform;
+        offset = Mathf.Abs(_playerTransform.position.z - transform.position.z);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(_playerTransform.position.x,_playerTransform.position.y, _playerTransform.position.z - offset);
+    }
+}
