61eb0e3d2be428b2bf7cd99fe6b1f06e6f75f8c7
Toster <tostermocy@gmail.com>
2020-01-25 21:48:44 +0100

    gowno

--- bibi sensei/Assets/PlayerMovement.cs
+++ bibi sensei/Assets/PlayerMovement.cs
@@ -1,18 +1,55 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    [SerializeField] private float MovementSpeed = 10.0f;
+    [SerializeField] private float JumpHeight = 10.0f;
+
+    private Rigidbody2D _rigidbody2D;
+    private Vector2 _movementDir;
+
+    private bool isOnGround = true;
+    
     // Start is called before the first frame update
     void Start()
     {
+        _rigidbody2D = gameObject.GetComponent<Rigidbody2D>();
+        _movementDir=new Vector2(0,0);
+    }
+    
+    private void FixedUpdate()
+    {
+        if(Input.GetKey(KeyCode.Space) || Input.GetKey(KeyCode.W))
+        {
+            if (isOnGround)
+            {
+                _rigidbody2D.velocity = (Vector2.up*JumpHeight);
+                isOnGround = false;
+            }
+        }
+
+       
+        if (Input.GetKey(KeyCode.D))
+        {
+            _rigidbody2D.velocity = (Vector2.right*MovementSpeed);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            _rigidbody2D.velocity = (Vector2.left*MovementSpeed);
+        }
         
+        _rigidbody2D.velocity = Vector2.Lerp(_rigidbody2D.velocity,Vector2.zero, 0.3f);
+
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnCollisionEnter2D(Collision2D other)
     {
-        
+        if (other.gameObject.CompareTag("Ground"))
+        {
+            isOnGround = true;
+        }
     }
 }
