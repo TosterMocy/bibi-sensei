a54b1ad9173cff0f5a25d59f9df1f95fbd44868a
Toster <tostermocy@gmail.com>
2020-01-26 01:15:11 +0100

    no message

---
+++ bibi sensei/Assets/EndGame.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using DG.Tweening;
+using TMPro;
+using UnityEngine;
+
+public class EndGame : MonoBehaviour
+{
+    [SerializeField] private GameObject endGameCanvas;
+
+    public CanvasGroup _canvasGroup;
+
+    private void Start()
+    {
+        _canvasGroup = endGameCanvas.GetComponent<CanvasGroup>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            endGameCanvas.SetActive(true);
+
+            _canvasGroup.DOFade(1f, 2f);
+
+        }
+    }
+}
